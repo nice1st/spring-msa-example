2d7451004cc6d109179aa1fd86a528a0041e9f39
nice1st <sasimi86cyh@gmail.com>
2020-10-15 17:51:32 +0900

    reactive rabbitmq

---
+++ msa-review/src/main/java/server/msareview/component/MessageProducer.java
@@ -0,0 +1,16 @@
+package server.msareview.component;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+@EnableBinding(MessageProducer.WriteReviewSource.class)
+public class MessageProducer {
+    
+    public interface WriteReviewSource {
+        String writeReview = "write-review";
+    
+        @Output(writeReview)
+        MessageChannel writeReview();
+    }
+}
--- msa-review/src/main/java/server/msareview/controller/ReviewController.java
+++ msa-review/src/main/java/server/msareview/controller/ReviewController.java
@@ -1,12 +1,28 @@
 package server.msareview.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import server.msareview.component.MessageProducer.WriteReviewSource;
 
 @RestController
 public class ReviewController {
+    
+    @Autowired
+    WriteReviewSource writeReviewSource;
+
     @RequestMapping("/")
     public String getReviews() {
         return "review.";
+    }
+
+    @RequestMapping(value="/", method = RequestMethod.POST)
+    public String setReview() {
+        // set new review
+        writeReviewSource.writeReview().send(MessageBuilder.withPayload("{seq : 13322}").build());
+        return "write review";
     }
 }
