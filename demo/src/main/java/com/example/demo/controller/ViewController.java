bd3403579296f189359fff20ce4170d21c0eb6d2
nice1st <sasimi86cyh@gmail.com>
2020-10-15 11:15:11 +0900

    gradle springboot demo

---
+++ demo/src/main/java/com/example/demo/controller/ViewController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+    @RequestMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @RequestMapping("/test")
+    public String test(Model model) {
+        return "index";
+    }
+}
